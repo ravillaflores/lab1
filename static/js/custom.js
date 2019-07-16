f5a9df6b7c540b5b6f4a3f4436b165f99c35b8aa
Raymond Anthony A Villaflores II <ravillaflores@student.apc.edu.ph>
2019-07-16 08:15:34 +0800

    no message

--- static/js/custom.js
+++ static/js/custom.js
@@ -13,8 +13,8 @@ function setFooterDate(target) {
 function toggleInfo(target) {
     var loc = $.trim(target.substring(5,20));
     var tgtID = '#info-'+loc;
-    alert(tgtID);
-    var chk = document.getElementById(target).hasAttribute("hidden");
+    //alert(tgtID);
+    var chk = document.getElementById($.trim(tgtID.substring(1,20))).hasAttribute("hidden");
     if(chk){
       $(tgtID).removeAttr('hidden');
     }
